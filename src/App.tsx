0b370c83a13e4e9f92be089274b3b4c50469be04
Serkan Polat <itsonlydatahustle@gmail.com>
2024-08-08 13:01:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,63 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+} from "react-router-dom";
 import Layout from "./components/Layout";
-import { AuthProvider } from "./context/Auth";
+import { AuthProvider, useAuth } from "./context/Auth";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Project from "./pages/Project";
 
-const App = () => {
+const App: React.FC = () => {
+  const PrivateRoute: React.FC<{ children: React.ReactElement }> = ({
+    children,
+  }) => {
+    const { userLoggedIn, loading } = useAuth();
+
+    if (loading) {
+      return (
+        <div className="h-screen w-screen bg-zinc-950">
+          <div className="h-full w-full flex flex-col justify-center items-center">
+            <p className="text-white">Loading...</p>
+          </div>
+        </div>
+      );
+    }
+
+    return userLoggedIn ? children : <Navigate to="/login" />;
+  };
+
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route index element={<Login />} />
-          <Route path="/" element={<Layout />}>
-            <Route path="/app" element={<Home />} />
-            <Route path="/project/:id" element={<Project />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Layout />
+              </PrivateRoute>
+            }
+          >
+            <Route
+              path="app"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="project/:id"
+              element={
+                <PrivateRoute>
+                  <Project />
+                </PrivateRoute>
+              }
+            />
           </Route>
         </Routes>
       </Router>
