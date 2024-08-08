93cddf54f1e6eca9f34136b1e303366a4fd522b1
Serkan Polat <itsonlydatahustle@gmail.com>
2024-08-08 13:01:24 +0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,11 +8,12 @@ import {
   Sheet,
   SheetClose,
   SheetContent,
-  SheetDescription,
   SheetHeader,
-  SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
+import { Button } from "./ui/button";
+import { signOut } from "firebase/auth";
+import { auth } from "@/config/firebase";
 
 type Project = {
   id: string;
@@ -30,9 +31,7 @@ const Layout = () => {
       try {
         setLoading(true);
 
-        const response = await getAllDocuments("projects");
-
-        console.log(response);
+        const response = (await getAllDocuments("projects")) as Project[];
 
         setList(response);
       } catch (error) {
@@ -46,28 +45,31 @@ const Layout = () => {
   }, []);
 
   const handleProjectSelect = (projectId: string) => {
-    console.log("PROJECTID: ", projectId);
     navigate(`/project/${projectId}`);
   };
 
   return (
-    <div className="h-screen w-screen flex flex-1 flex-col">
-      <header className="bg-gray-800 text-white p-4 gap-4 flex items-center">
+    <div className="h-screen w-screen flex flex-1 flex-col bg-white">
+      <header className="bg-white text-blue-600 p-4 gap-4 flex items-center shadow-md">
         <Sheet>
           <SheetTrigger asChild>
             <button>
-              <IconMenu2 />
+              <IconMenu2 className="text-blue-600" />
             </button>
           </SheetTrigger>
           <SheetContent side={"left"}>
             <SheetHeader>
-              <SheetTitle>YZF Automations</SheetTitle>
-              <SheetDescription>
-              </SheetDescription>
+              <div className="flex flex-col items-center justify-center">
+                <img
+                  src="/images/logo.png"
+                  alt="Logo"
+                  className="w-48 h-32 object-contain"
+                />
+              </div>
             </SheetHeader>
             {loading ? (
               <div className="w-full h-full flex justify-center items-center">
-                <Icons.spinner className="h-8 w-8 animate-spin" />
+                <Icons.spinner className="h-8 w-8 animate-spin text-blue-600" />
               </div>
             ) : (
               <ScrollArea>
@@ -80,7 +82,7 @@ const Layout = () => {
                           onClick={() =>
                             handleProjectSelect(item.id.toString())
                           }
-                          className="col-span-3 bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-md"
+                          className="bg-gray-200 hover:bg-gray-300 px-4 py-2 rounded-md text-blue-600"
                         >
                           {item.name}
                         </button>
@@ -92,21 +94,33 @@ const Layout = () => {
             )}
           </SheetContent>
         </Sheet>
-        <h1
-          className="text-xl font-semibold cursor-pointer hover:underline"
-          onClick={() => navigate("/app")}
-        >
-          YZF
-        </h1>
+        <div className="flex flex-1 flex-row justify-between items-center">
+          <button
+            className="text-2xl font-semibold cursor-pointer hover:underline text-blue-600"
+            onClick={() => navigate("/app")}
+          >
+            YZF
+          </button>
+
+          <Button
+            onClick={() => {
+              signOut(auth);
+              navigate("/");
+            }}
+            className="bg-blue-500 hover:bg-blue-600 text-white"
+          >
+            Çıkış Yap
+          </Button>
+        </div>
       </header>
 
       <main className="h-full w-full flex-1">
         <Outlet />
       </main>
 
-      <footer className="bg-black p-2">
+      <footer className="bg-white p-2">
         <div className="flex flex-col justify-center items-center gap-2">
-          <p className="text-white font-bold">&copy; YZF</p>
+          <p className="text-blue-600 font-bold">&copy; YZF</p>
         </div>
       </footer>
     </div>
